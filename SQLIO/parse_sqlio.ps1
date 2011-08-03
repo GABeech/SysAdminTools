4658772fd95cca126b44d576394d2af11c1109a7
George Beech <gbeech@gmail.com>
2011-08-03 00:10:14 -0400

    started working on powershell port .. header only ... bad motivation day

---
+++ SQLIO/parse_sqlio.ps1
@@ -0,0 +1,11 @@
+# parse_sqlio.ps1
+# Author: George Beech <george@stackexchange.com> 
+#
+# This is a powershell conversion of the perl SQLIO conversion script. 
+# I takes a SQLIO output file and converts it to a csv file
+#
+#
+#
+#######################################################################
+
+
