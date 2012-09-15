1808becf6a1e341588ddbf55b8ec55588a81839e
George Beech <gbeech@gmail.com>
2012-09-15 14:08:49 -0400

    Fixed bug with the hg status command, added command line opts to the bash version

--- mercurial/auto_commiter.sh
+++ mercurial/auto_commiter.sh
@@ -23,6 +23,13 @@ REPO_ROOT="/srv/tftp/cisco"
 # first lets make sure we are in the REPO Root
 cd $REPO_ROOT
 
+while getopts "r:" args do
+do
+	case $args in
+		 d) REPO_ROOT="$OPTARG";;
+	esac
+done
+
 # Check to see if there are incoming changes that we should pull first
 hg incoming
 # If there are changes pull the latest from the server to make sure we have the lateest
@@ -32,21 +39,14 @@ then
 	if [ $? -eq 1 ]
 	then
 		#TODO: Need to send an exception email or use logger?
+		logger -p local4.warn -t "[AutoCommiter]" "Unable to pull from repo. Please check the Repo and the config"
 		exit 1
 	fi
 fi
 
 # Check to see if there are any updates that need to be pushed
-if [ $(hg status --rev | wc -l) -ne 0 ]
+if [ $(hg status --rev tip -m -a| wc -l) -ne 0 ]
 then
 	hg commit -m "Auto Commit by commit script. Changes Detected" -A
 	hg push
 fi
-
-
-
-
-
-
-
-
