c4f85ea2f71a6e4bc609cb4cdbbb5815bc527a87
George Beech <george@brokenhaze.com>
2013-02-21 17:32:17 -0500

    Added locking
    This prevents dueling backups when you run on a more frequent schedule

--- Network/backup_asa_config.sh
+++ Network/backup_asa_config.sh
@@ -3,11 +3,26 @@
 #ASA_IP="10.7.0.1"
 ASA_IP=$1
 CONFIG_PATH=$2
+LOCK_DIR="/tmp/network_backup/"
+LOCK_FILE="${ASA_IP}.lock"
 
-if [ ! -d ${CONFIG_PATH} ]
+if [ ! -d ${LOCK_DIR} ]
 then
-	mkdir -p ${CONFIG_PATH}
+	mkdir -p ${LOCK_DIR}
 fi
-/usr/bin/wget --no-check-certificate --http-user=<asa_user> --http-password=<asa_password> -O ${CONFIG_PATH}/config https://${ASA_IP}/config
-sed -i -e '1d' ${CONFIG_PATH}/config
-sed -i -e '1d' ${CONFIG_PATH}/config
+
+if [ -e ${LOCK_DIR}${LOCK_FILE} ]
+then
+	exit
+else
+	touch ${LOCK_DIR}${LOCK_FILE}
+	if [ ! -d ${CONFIG_PATH} ]
+	then
+		mkdir -p ${CONFIG_PATH}
+	fi
+	/usr/bin/wget --no-check-certificate --http-user=svc_asabackup --http-password=55kEahCRyrpSwXuR8OTY -O ${CONFIG_PATH}/config https://${ASA_IP}/config
+	sed -i -e '1d' ${CONFIG_PATH}/config
+	sed -i -e '1d' ${CONFIG_PATH}/config
+fi
+
+rm ${LOCK_DIR}${LOCK_FILE}
